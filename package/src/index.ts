63cd60ef9c5e94516057b477554a45e3e06c954f
CozyBrian <dragonbrian33@gmail.com>
2023-01-28 19:30:08 +0000

    package: 0.0.4

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,22 +1,21 @@
 import axios from "axios";
-import dns from "dns";
-import os from "os";
 
 class Sitenary {
   private siteId: string;
 
   constructor (site: string) {
     this.siteId = site;
-
     console.log("Sitenary is running...");
     (async () => {
       this.siteId = site;
-      const clientIp = await axios.get(`http://localhost:3001/v1/ip`);
+
+      const clientIpRes = await fetch(`https://sitenary-web-huuh3.ondigitalocean.app/v1/ip`);
+      const clientIp = await clientIpRes.json();
       console.log(`Sitenary: sending event`);
-      return axios.post(`http://localhost:3001/v1/events/${this.siteId}`, {
+      return axios.post(`https://sitenary-web-huuh3.ondigitalocean.app/v1/events/${this.siteId}`, {
         event: {
           type: "VIEW",
-          ip: `${clientIp.data.ip}`,
+          ip: `${clientIp.ip}`,
           platform: "MacOs"
         }
       })
@@ -30,26 +29,6 @@ class Sitenary {
       })}
     )();
   }
-
-  private async getIp () {
-    console.log("Sitenary: Getting IP address...");
-    return new Promise((resolve, reject) => {
-      dns.lookup(os.hostname(),(err: NodeJS.ErrnoException | null, address: string, fam: number) => {
-        if (err) {
-          console.log("Sitenary: Error getting IP address!");
-          reject(err);
-        } else {
-          console.log("Sitenary: IP address found!");
-          resolve(address);
-        }
-      });
-    });
-  }
 }
-
-const sitenary = new Sitenary("63d322b993638313b3f65b27");
-
-
-
 
 export default Sitenary;
