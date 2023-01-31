8c99bfaa3bde650eeea5aaacac6cd578fd74cab4
CozyBrian <dragonbrian33@gmail.com>
2023-01-31 19:04:05 +0000

    harder to post trash sites

--- server/src/routes/sites/sites.controller.ts
+++ server/src/routes/sites/sites.controller.ts
@@ -13,19 +13,25 @@ export const getSites = (req: Request, res: Response) => {
 export const postSite = async (req: Request, res: Response) => {
   const { name, url } = req.body;
 
+  const regex = new RegExp("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$");
+
   if (await isSiteExists(url)) {
     res.status(400).json({ message: "Site already exists" });
   } else {
     if ((name === "" || name === undefined) || (url === "" || url === undefined)) {
       res.status(400).json({ message: "Missing Field is required" });
     } else {
-      createNewSite({ name, url })
-      .then((site) => {
-        res.status(201).json(site);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-      });
+      if (regex.test(url) === false) {
+        res.status(400).json({ message: "Invalid URL" });
+      } else {
+        createNewSite({ name, url })
+        .then((site) => {
+          res.status(201).json(site);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+      }
     }
   }
 
