6863d0439f9a917f7d7c98a03efda7c49911bb9c
CozyBrian <dragonbrian33@gmail.com>
2023-02-11 23:26:56 +0000

    removed unused socket

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,11 @@
 require('dotenv').config();
 import http from "http";
 import app from "./app";
-import { Server } from "socket.io";
 import MongoInit from "./mongo";
-import SocketHandler from "./sockets";
 
 const PORT = process.env.PORT || 3001;
 
 const server = http.createServer(app);
-
-export const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
-
-io.on("connection", SocketHandler);
-
 
 function startServer() {
   server.listen(PORT, () => {
--- server/src/sockets/index.ts
+++
@@ -1,9 +0,0 @@
-import { Socket } from "socket.io";
-import { DefaultEventsMap } from "socket.io/dist/typed-events";
-
-const SocketHandler = (socket:Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) => {
-  console.log(`a user Connected - ${socket.id}`);
-
-};
-
-export default SocketHandler;
