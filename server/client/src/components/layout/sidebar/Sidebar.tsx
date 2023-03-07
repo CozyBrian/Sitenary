4ea02f0cc105c2e78d8298863211c188390730f5
CozyBrian <dragonbrian33@gmail.com>
2023-03-07 18:46:14 +0000

    sidebar

--- server/client/src/components/layout/sidebar/Sidebar.tsx
+++ server/client/src/components/layout/sidebar/Sidebar.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Oval } from "react-loader-spinner";
 import Close from "../../../assets/icons/close.svg";
 import { useQuery } from "react-query";
+import { useEventListener } from "usehooks-ts";
 import { useAppDispatch, useAppSelector } from "../../../hooks";
 import { ISite } from "../../../types";
 import { getSites } from "../../../utils/Sitenary";
@@ -25,39 +26,70 @@ const Sidebar = () => {
     refetchOnWindowFocus: false,
   });
 
+  const resizeRef = React.useRef<HTMLDivElement>(null);
+  const [isResizing, setIsResizing] = useState(false);
+
+  const handleMouseDown = (e: MouseEvent) => {
+    setIsResizing(true);
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    e.preventDefault();
+    if (isResizing) {
+      if (299 < e.pageX && e.pageX < 420) {
+        document.body.style.setProperty("--SidebarWidth", `${e.pageX}px`);
+      }
+    }
+  };
+
+  const handleMouseUp = (e: MouseEvent) => {
+    setIsResizing(false);
+  };
+
+  useEventListener("mousedown", handleMouseDown, resizeRef);
+  useEventListener("mousemove", handleMouseMove);
+  useEventListener("mouseup", handleMouseUp);
+
   return (
-    <nav className="main-sidebar">
-      <div className="main-sidebar-header">
-        <h1>Sitenary</h1>
-        <div
-          className="main-sidebar-icon"
-          onClick={() => dispatch(action.app.setAddSiteModalOpen(true))}
-        >
-          <img src={Close} alt="close-icon" />
+    <div className="sidebar-wrapper">
+      <nav className="main-sidebar">
+        <div className="main-sidebar-header">
+          <h1>Sitenary</h1>
+          <div
+            className="main-sidebar-icon"
+            onClick={() => dispatch(action.app.setAddSiteModalOpen(true))}
+          >
+            <img src={Close} alt="close-icon" />
+          </div>
         </div>
+        <div className="main-sidebar-list">
+          {isError && (
+            <div className="loading-container">Error loading sites...</div>
+          )}
+          {isLoading ? (
+            <div className="loading-container">
+              <Oval
+                color="#737373"
+                secondaryColor="#D7D7D7"
+                width={42}
+                strokeWidth={4}
+              />
+            </div>
+          ) : data !== undefined && data.length !== 0 ? (
+            data.map((site: ISite) => (
+              <SidebarItem key={site._id} site={site} />
+            ))
+          ) : (
+            <div className="empty">
+              <p>No Sites</p>
+            </div>
+          )}
+        </div>
+      </nav>
+      <div ref={resizeRef} className="sidebar-resize-handle">
+        <div className="resize-inner" />
       </div>
-      <div className="main-sidebar-list">
-        {isError && (
-          <div className="loading-container">Error loading sites...</div>
-        )}
-        {isLoading ? (
-          <div className="loading-container">
-            <Oval
-              color="#737373"
-              secondaryColor="#D7D7D7"
-              width={42}
-              strokeWidth={4}
-            />
-          </div>
-        ) : data !== undefined && data.length !== 0 ? (
-          data.map((site: ISite) => <SidebarItem key={site._id} site={site} />)
-        ) : (
-          <div className="empty">
-            <p>No Sites</p>
-          </div>
-        )}
-      </div>
-    </nav>
+    </div>
   );
 };
 
