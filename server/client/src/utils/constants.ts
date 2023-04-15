4dc3467b11c1cece5cfe22a81e7391e127fce20b
CozyBrian <dragonbrian33@gmail.com>
2023-04-15 23:04:12 +0000

    multi-domain

--- server/client/src/utils/constants.ts
+++ server/client/src/utils/constants.ts
@@ -1,7 +1,5 @@
 export const BACKEND_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3001"
-    : "https://sitenary-web-huuh3.ondigitalocean.app";
+  process.env.NODE_ENV === "development" ? "http://localhost:3001" : "";
 
 export const urlRegex = new RegExp(
   "^(http://www.|https://www.|http://|https://)?[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$"
